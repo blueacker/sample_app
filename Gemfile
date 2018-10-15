1bc9c9d32dfe38665f62a1e7460634d6ea3eb869
Zijun Zhang <blueacker@gmail.com>
2018-10-16 06:10:29 +0800

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails',        '~> 5.0.0'
+gem 'rails',        '~> 5.1.0'
 gem 'bootstrap-sass', '3.3.6'
 gem 'pg',           '0.18.4'
 # Use Puma as the app server
@@ -26,6 +26,9 @@ gem 'jbuilder',     '~> 2.5'
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.11'
+gem 'faker'
+gem 'will_paginate'
+gem 'bootstrap-will_paginate'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -20,6 +20,7 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: session[:user_id])
@@ -32,7 +33,20 @@ module SessionsHelper
     end
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def logged_in?
     ! current_user.nil?
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
